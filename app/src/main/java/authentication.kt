00b0cb7bb158dff070c4619d587b2a8a6d2bb30d
nimo247 <n14082005@gmail.com>
2024-10-25 00:17:56 +0530

    Create authentication.kt

---
+++ app/src/main/java/authentication.kt
@@ -0,0 +1,84 @@
+package com.example.passmng
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.passmng.databinding.ActivityAuthenticationBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_authentication.*
+
+class Authentication : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthenticationBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.signup.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginEmail.text.toString()
+            val pass = binding.loginPassword.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    /*override fun onStart() {
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }*/
+
+
+
+
+
+        /*login_button.setOnClickListener {
+            var email_auth = login_email.text.toString()
+            var pass_auth = login_password.text.toString()
+
+            if(email_auth == "test@admin.com" && pass_auth == "12345678"){
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(getBaseContext(), "Invalid credentials...Hint: Use credentials as mentioned \n in GDSC NSUT Recruitment Task Document", Toast.LENGTH_LONG).show()
+            }
+
+        }*/
+
+
+
+
+
+
+}
