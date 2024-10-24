9e806f1cde925f7e6f2a7f9e50b19a3a2ccbf695
nimo247 <n14082005@gmail.com>
2024-10-25 00:24:53 +0530

    Create SplashScreen.kt

---
+++ app/src/main/java/SplashScreen.kt
@@ -0,0 +1,36 @@
+package com.example.passmng
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.room.Room
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var database: myDatabase
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        //fetching data
+        database = Room.databaseBuilder(
+            applicationContext, myDatabase::class.java, "Pwd_Manager"
+        ).build()
+        GlobalScope.launch {
+            DataObject.listdata = database.dao().getTasks() as MutableList<CardInfo>
+        }
+        //done
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Authentication::class.java)
+            startActivity(intent)
+        }, 2000)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        val intent = Intent(this, Authentication::class.java)
+        startActivity(intent)
+    }
+}
